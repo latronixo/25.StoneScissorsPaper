5529e07592e9a6bcf095055deb0869d1eeecf623
Valentin <latronixo@gmail.com>
2025-05-21 10:39:43 +0300

    Initial Commit

---
+++ 25.StoneScissorsPaper/_5_StoneScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_StoneScissorsPaperApp.swift
+//  25.StoneScissorsPaper
+//
+//  Created by Валентин on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct _5_StoneScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
