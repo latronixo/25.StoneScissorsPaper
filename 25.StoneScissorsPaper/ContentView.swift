f2f148fa08b00d8e4f80a6ddeeaa2a6d88e64df3
Valentin <latronixo@gmail.com>
2025-05-21 12:23:08 +0300

    Нужно в зависимости от shouldWin возвращать одно из двух различных Text View

--- 25.StoneScissorsPaper/ContentView.swift
+++ 25.StoneScissorsPaper/ContentView.swift
@@ -8,17 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    //варианты жестов
+    private var moves = ["🪨", "📜", "✂"]
+    //жесты-победители
+    private var winningMoves = ["📜", "✂", "🪨"]
+    
+    //жест приложения
+    @State private var appMove = ""
+    @State private var score = 0
+    @State private var shouldWin = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Ваш ход:")
+                .font(.title.bold())
+            Spacer()
+            HStack{
+                ForEach(moves, id: \.self) { move in
+                    Button {
+                        moveTapped(move)
+                    } label: {
+                        Text(move)
+                            .font(.system(size: 100))
+                    }
+                }
+            }
+            Spacer()
         }
-        .padding()
+        .padding(10)
+        .onAppear{
+            nextRound()
+            shouldWin = Bool.random()
+        }
+        VStack {
+            Text("Ваш счет: \(score)")
+            Text("Ход приложения:")
+            HStack{
+                Text(appMove)
+                    .font(.system(size: 100))
+            }
+            Text("Вам нужно:")
+
+            Spacer()
+
+        }
+    }
+    private func moveTapped(_ userMove: String) {
+        let indexUserMove = moves.firstIndex(of: userMove)
+        let indexAppMove = moves.firstIndex(of: appMove)
+        if indexUserMove == indexAppMove {
+            print("победа")
+        } else {
+            print("поражение")
+        }
+        nextRound()
+        //print ("индекс выбранного пользователем жеста \(indexUserMove)")
+    }
+    
+    private func nextRound() {
+        appMove = moves[Int.random(in: 0...2)]
+        shouldWin.toggle()
+        print(appMove)
     }
 }
-
+    
 #Preview {
     ContentView()
 }
